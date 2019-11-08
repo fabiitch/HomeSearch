e54dab4f9f2c21dfa9d38ae6562a5391498a5a0b
foccelli <foccelli@reservit.com>
2019-11-08 09:47:25 +0100

    start project

--- core/src/com/nzt/homesearch/HomeSearchMain.java
+++ core/src/com/nzt/homesearch/HomeSearchMain.java
@@ -1,19 +1,39 @@
 package com.nzt.homesearch;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net.HttpMethods;
+import com.badlogic.gdx.Net.HttpRequest;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.nzt.gdx.net.AutoProxy;
+import com.nzt.gmaps.GMaps;
+import com.nzt.gmaps.TextureDownloader;
 
-public class HomeSearchMain extends ApplicationAdapter {
+import NztGMaps.NzMapsLib;
+
+public class HomeSearchMain extends ApplicationAdapter implements Observer {
 	SpriteBatch batch;
 	Texture img;
+	NzMapsLib z;
 	
 	@Override
 	public void create () {
+		AutoProxy.init();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		TextureDownloader tDow = new TextureDownloader();
+		tDow.addObserver(this);
+//		tDow.downloadImage(url);
+		
+	GMaps.geoLocate();
+		
+//		https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
 	}
 
 	@Override
@@ -29,5 +49,11 @@ public class HomeSearchMain extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("finish dowload image");
+		
 	}
 }
